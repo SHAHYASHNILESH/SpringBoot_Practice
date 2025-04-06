35a3bde4abf10239c8ee82501cd2ca0d74849f7f
SHAHYASHNILESH <nileshshah0409@yahoo.co.in>
2025-04-06 22:03:45 +0530

    Added new Pattern

--- PatternsDemo/src/com/starPatterns/allInOne/PatternPractice.java
+++ PatternsDemo/src/com/starPatterns/allInOne/PatternPractice.java
@@ -97,8 +97,7 @@ class PatternPractice {
 
 			System.out.println();
 		}
-		
-	
+
 	}
 
 	public static void reverseNumberTrianglePattern(int n) {
@@ -260,110 +259,187 @@ class PatternPractice {
 		}
 	}
 
+	public static void diamondPattern2(int n) {
+
+		for (int i = 1; i < n; i++) {
+
+			for (int j = i; j <= n; j++) {
+
+				System.out.print("*");
+
+			}
+
+			for (int j = 1; j < i; j++) {
+
+				System.out.print(" ");
+
+			}
+
+			for (int j = 1; j < i; j++) {
+
+				System.out.print(" ");
+
+			}
+
+			for (int j = i; j <= n; j++) {
+
+				System.out.print("*");
+
+			}
+
+			System.out.println();
+
+		}
+
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = 1; j <= i; j++) {
+
+				System.out.print("*");
+
+			}
+
+			for (int j = i; j < n; j++) {
+
+				System.out.print(" ");
+
+			}
+
+			for (int j = i; j < n; j++) {
+
+				System.out.print(" ");
+
+			}
+
+			for (int j = 1; j <= i; j++) {
+
+				System.out.print("*");
+
+			}
+
+			System.out.println();
+
+		}
+
+	}
+
 	public static void main(String[] args) {
 
-		//	      1 
-		//	     2 2 
-		//	    3 3 3 
-		//	   4 4 4 4 
-		//	  5 5 5 5 5 
+		// 1
+		// 2 2
+		// 3 3 3
+		// 4 4 4 4
+		// 5 5 5 5 5
 		System.out.println("++++++++++++++++++++++++++++++++++++++");
 		numberTrianglePattern(5);
 
-		//		1 
-		//		2 3 
-		//		4 5 6 
-		//		7 8 9 10 
-		//		11 12 13 14 15 
+		// 1
+		// 2 3
+		// 4 5 6
+		// 7 8 9 10
+		// 11 12 13 14 15
 		System.out.println("++++++++++++++++++++++++++++++++++++++");
 		numberChangingPattern(5);
 
-		//	     *
-		//	    ***
-		//	   *****
-		//	  *******
-		//	 *********
+		// *
+		// ***
+		// *****
+		// *******
+		// *********
 		System.out.println("++++++++++++++++++++++++++++++++++++++");
 		palindromeTrianglePattern(5);
 
-		//		*********
-		//		 *******
-		//		  *****
-		//		   ***
-		//		    *
+		// *********
+		// *******
+		// *****
+		// ***
+		// *
 		System.out.println("++++++++++++++++++++++++++++++++++++++");
 		palindromeReverseTrianglePattern(5);
 
-		//		     *
-		//		    ***
-		//		   *****
-		//		  *******
-		//		 *********
-		//		  *******
-		//		   *****
-		//		    ***
-		//		     *
+		// *
+		// ***
+		// *****
+		// *******
+		// *********
+		// *******
+		// *****
+		// ***
+		// *
 		System.out.println("++++++++++++++++++++++++++++++++++++++");
 		diamondPattern(5);
 
-		//		     *
-		//		    **
-		//		   ***
-		//		  ****
-		//		 *****
+		// *
+		// **
+		// ***
+		// ****
+		// *****
 		System.out.println("++++++++++++++++++++++++++++++++++++++");
 		increasingRightAngledTriangle(5);
 
-		//		*****
-		//		 ****
-		//		  ***
-		//		   **
-		//		    *
+		// *****
+		// ****
+		// ***
+		// **
+		// *
 		System.out.println("++++++++++++++++++++++++++++++++++++++");
 		decreasingRightAngledTriangle(5);
 
-		//		 1 2 3 4 5 
-		//		  2 3 4 5 
-		//		   3 4 5 
-		//		    4 5 
-		//		     5 
+		// 1 2 3 4 5
+		// 2 3 4 5
+		// 3 4 5
+		// 4 5
+		// 5
 		System.out.println("++++++++++++++++++++++++++++++++++++++");
 		reverseNumberTrianglePattern(5);
 
-		//		      1 
-		//		     1 1 
-		//		    1 2 1 
-		//		   1 3 3 1 
-		//		  1 4 6 4 1 
+		// 1
+		// 1 1
+		// 1 2 1
+		// 1 3 3 1
+		// 1 4 6 4 1
 		System.out.println("++++++++++++++++++++++++++++++++++++++");
 		pascalTriangle(5);
 
-		//		      * 
-		//		     * * 
-		//		    * * * 
-		//		   * * * * 
-		//		  * * * * * 
+		// *
+		// * *
+		// * * *
+		// * * * *
+		// * * * * *
 		System.out.println("++++++++++++++++++++++++++++++++++++++");
 		increasingTrianglePattern(5);
 
-		//		 * * * * * 
-		//		  * * * * 
-		//		   * * * 
-		//		    * * 
-		//		     * 
+		// * * * * *
+		// * * * *
+		// * * *
+		// * *
+		// *
 		System.out.println("++++++++++++++++++++++++++++++++++++++");
 		decreasingTrianglePattern(5);
 
-		//		 * * * * * 
-		//		  * * * * 
-		//		   * * * 
-		//		    * * 
-		//		     * 
-		//		    * * 
-		//		   * * * 
-		//		  * * * * 
-		//		 * * * * * 
+		// * * * * *
+		// * * * *
+		// * * *
+		// * *
+		// *
+		// * *
+		// * * *
+		// * * * *
+		// * * * * *
 		System.out.println("++++++++++++++++++++++++++++++++++++++");
 		trianglePattern(5);
+
+		//		**********
+		//		****  ****
+		//		***    ***
+		//		**      **
+		//		*        *
+		//		**      **
+		//		***    ***
+		//		****  ****
+		//		**********
+		System.out.println("++++++++++++++++++++++++++++++++++++++");
+		diamondPattern2(5);
+		
 	}
 }
