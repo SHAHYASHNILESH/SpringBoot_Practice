d495f912169699aad14590669723e79340dfe22f
SHAHYASHNILESH <nileshshah0409@yahoo.co.in>
2025-12-09 19:58:18 +0530

    Initial Commit

---
+++ tender_management_system/src/main/java/com/tender_management/tender_management_system/TenderManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.tender_management.tender_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenderManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenderManagementSystemApplication.class, args);
+        System.out.println("running....");
+    }
+
+}
---
+++ tender_management_system/src/test/java/com/tender_management/tender_management_system/TenderManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tender_management.tender_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenderManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
